a2702aba366f4012904e528d53daca5928a2ddd0
Ahmad Kabalan <ahmadk@Ahmads-MacBook-Air.local>
2025-03-14 16:00:27 -0400

    Ajout des classes utilitaires et bd-capteurs

---
+++ config/utilitaire.js
@@ -0,0 +1,19 @@
+class Utilitaire {
+    // floor cest pour arrondir vers le bas pour que le caractere reste in bounds math random retourne un nombre entre 0 et 1
+            // par le length et on refait sa 30 fois
+    static creationCleAPI() {
+        const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const longueurCle = 30;
+        let cle = '';
+ 
+        for (let i = 0; i < longueurCle; i++) {
+            const randomIndex = Math.floor(Math.random() * caracteres.length);
+            cle += caracteres[randomIndex];
+        }
+ 
+        return cle;
+    }
+}
+ 
+module.exports = Utilitaire;
+ 
